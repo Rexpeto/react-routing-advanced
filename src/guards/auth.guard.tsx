46f935517c4035b67446a410d12c7224382eec06
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-17 03:13:10 -0400

    Guards routes

---
+++ src/guards/auth.guard.tsx
@@ -0,0 +1,16 @@
+import { PublicRoutes } from "@/models";
+import { AppStore } from "@/redux/store";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const AuthGuard = () => {
+  const userState = useSelector((store: AppStore) => store.user);
+
+  return userState.name ? (
+    <Outlet />
+  ) : (
+    <Navigate replace to={PublicRoutes.LOGIN} />
+  );
+};
+
+export default AuthGuard;
