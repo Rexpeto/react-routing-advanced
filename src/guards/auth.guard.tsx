79f7ac1441460f29e439973661b07683f45fba32
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-18 15:47:59 -0400

    private route protection

--- src/guards/auth.guard.tsx
+++ src/guards/auth.guard.tsx
@@ -1,13 +1,26 @@
-import { PublicRoutes } from "@/models";
+import { PrivateRoutes, PublicRoutes } from "@/models";
 import { AppStore } from "@/redux/store";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-const AuthGuard = () => {
+const PublicValidateFragment = (
+  <Navigate replace to={`${PrivateRoutes.DASHBOARD}`} />
+);
+const PrivateValidateFragment = <Outlet />;
+
+interface Props {
+  privateValidation: boolean;
+}
+
+const AuthGuard = ({ privateValidation }: Props) => {
   const userState = useSelector((store: AppStore) => store.user);
 
   return userState.name ? (
-    <Outlet />
+    privateValidation ? (
+      PrivateValidateFragment
+    ) : (
+      PublicValidateFragment
+    )
   ) : (
     <Navigate replace to={PublicRoutes.LOGIN} />
   );
